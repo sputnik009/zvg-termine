daa939b5f469632d465e6a8793c5e9b10fd6460c
sputty <sputnik009@yahoo.de>
2016-12-24 10:31:43 +0100

    store search settings in session + use it at get

--- src/main/java/de/magic/creation/search/SearchController.java
+++ src/main/java/de/magic/creation/search/SearchController.java
@@ -7,12 +7,15 @@ import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
 
 import de.magic.creation.home.SearchSettings;
 
 @Controller
+@SessionAttributes("searchSettings")
 public class SearchController
 {
   private final Logger  log = LoggerFactory.getLogger( SearchController.class);
@@ -26,12 +29,35 @@ public class SearchController
   }
 
   @PostMapping("/search")
-  public String searchSubmit( @ModelAttribute SearchSettings settings, Model model)
+  public String searchSubmit( @ModelAttribute("searchSettings") SearchSettings settings, Model model)
   {
-    log.debug( "Search: " + settings);
+    log.debug( "searchSubmit (post)");
+    return searchInternal( settings, model);
+  }
+
+  @GetMapping("/search")
+  public String searchSession( @ModelAttribute("searchSettings") SearchSettings settings, Model model)
+  {
+    log.debug( "searchSubmit (get)");
+    return searchInternal( settings, model);
+  }
+
+  private String searchInternal( SearchSettings settings, Model model)
+  {
+    log.debug( "Search (internal): " + settings);
+    model.addAttribute( "searchSettings", settings);
+
     List<ZvgObject> objs = searchManager.search( settings);
+
     log.debug( "Search returns: " + objs.size());
     model.addAttribute( "objects", objs);
+
     return "searchResult";
+  }
+
+  @ModelAttribute("searchSettings")
+  public SearchSettings getSearchSettings()
+  {
+    return new SearchSettings();
   }
 }
